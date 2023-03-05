8fd70ab5af2e67bf7ee50237425444c333883af5
SnakeMacbookPro <snakemacbookpro@SnakeMacbookPro-iMac-Pro.local>
2023-03-05 20:52:03 +0100

    user data collection for the form submit-event was implemented

--- frontend/public/app.js
+++ frontend/public/app.js
@@ -1,9 +1,30 @@
 "use strict";
 const url = `http://localhost:5000/api/orders`;
-
 const form = document.querySelector(".needs-validation");
 const nameInput = document.getElementById("name");
 const addressInput = document.getElementById("address");
 const baseInputs = document.querySelectorAll('[name="base"]');
+function getBase() {
+    let base;
+    baseInputs.forEach((checkbox) => {
+        if (checkbox.checked) {
+            base = checkbox.value;
+        }
+        else {
+            base = "";
+        }
+    });
+    return base;
+}
 const toppingSelect = document.querySelector("select");
 const alertSuccess = document.querySelector(".alert-success");
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let base = getBase();
+    const data = {
+        name: nameInput.value,
+        address: addressInput.value,
+        base: base,
+        topping: toppingSelect.value,
+    };
+});
--- frontend/src/app.ts
+++ frontend/src/app.ts
@@ -4,7 +4,36 @@ const form: HTMLFormElement = document.querySelector(".needs-validation") as HTM
 const nameInput: HTMLInputElement = document.getElementById("name") as HTMLInputElement;
 const addressInput: HTMLInputElement = document.getElementById("address") as HTMLInputElement;
 const baseInputs: NodeListOf<HTMLInputElement> = document.querySelectorAll('[name="base"]');
-const toppingSelect: HTMLSelectElement  = document.querySelector("select") as HTMLSelectElement;
-const alertSuccess: HTMLElement = document.querySelector(".alert-success") as HTMLElement;
 
+function getBase() {
+  let base: string | undefined;
 
+  baseInputs.forEach((checkbox) => {
+    if (checkbox.checked) {
+      base = checkbox.value;
+    }else{
+      base = "";
+    }
+  });
+    return base;
+}
+const toppingSelect: HTMLSelectElement = document.querySelector(
+  "select"
+) as HTMLSelectElement;
+const alertSuccess: HTMLElement = document.querySelector(
+  ".alert-success"
+) as HTMLElement;
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  let base = getBase();
+
+  const data = {
+    name: nameInput.value,
+    address: addressInput.value,
+    base: base,
+    topping: toppingSelect.value,
+  };
+
+});
