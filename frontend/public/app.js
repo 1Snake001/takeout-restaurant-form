efce3017983b6ad7573e62935dcef6342f7dc16b
SnakeMacbookPro <snakemacbookpro@SnakeMacbookPro-iMac-Pro.local>
2023-03-05 20:06:33 +0100

    form & input dom elements was implemented

--- frontend/public/app.js
+++ frontend/public/app.js
@@ -1,1 +1,9 @@
 "use strict";
+const url = `http://localhost:5000/api/orders`;
+
+const form = document.querySelector(".needs-validation");
+const nameInput = document.getElementById("name");
+const addressInput = document.getElementById("address");
+const baseInputs = document.querySelectorAll('[name="base"]');
+const toppingSelect = document.querySelector("select");
+const alertSuccess = document.querySelector(".alert-success");
--- frontend/src/app.ts
+++ frontend/src/app.ts
@@ -1,3 +1,10 @@
+const url: string = `http://localhost:5000/api/orders`;
 
+const form: HTMLFormElement = document.querySelector(".needs-validation") as HTMLFormElement;
+const nameInput: HTMLInputElement = document.getElementById("name") as HTMLInputElement;
+const addressInput: HTMLInputElement = document.getElementById("address") as HTMLInputElement;
+const baseInputs: NodeListOf<HTMLInputElement> = document.querySelectorAll('[name="base"]');
+const toppingSelect: HTMLSelectElement  = document.querySelector("select") as HTMLSelectElement;
+const alertSuccess: HTMLElement = document.querySelector(".alert-success") as HTMLElement;
 
 
